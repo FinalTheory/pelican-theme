40765a49f942d2ea9ab03f9f2e58f1a93d2d2aad
FinalTheory <FinalTheory@hotmail.com>
2026-01-15 12:07:59 +0800

    update to latest

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,24 +6,24 @@
 
 
 from __future__ import unicode_literals
-from markdown.extensions import headerid, codehilite
 from collections import OrderedDict
 import datetime
-import md5
+import hashlib
 import os
+
 
 ###################################################################################################
 # 系统的全局设置
 
 # 名称以及站点相关
 AUTHOR = u'FinalTheory'
-SITENAME = u'FinalTheory\u7684\u6280\u672f\u611f\u609f'
-HOMENAME = u'首页'
+SITENAME = u'黄闻天的技术笔记'
+HOMENAME = '首页'
 # HOMEURL = ''
 SITEURL = ''
 
 # 时间与区域
-DEFAULT_LANG = u'zh_CN'
+DEFAULT_LANG = 'zh'
 TIMEZONE = 'Asia/Shanghai'
 DEFAULT_DATE = 'fs'
 UPDATEDATE_MODE = 'metadata'
@@ -44,12 +44,44 @@ PLUGINS = [
     'gravatar',
     'extract_headings',
     'niux2_hermit_player',
-    'niux2_lazyload_helper',
     'pelican-update-date',
     'sitemap',
     'summary',
     'render_math',
+    'i18n_subsites'
 ]
+
+NIUX2_HEADER_DROPDOWN_SECTIONS_EN = OrderedDict()
+NIUX2_HEADER_DROPDOWN_SECTIONS_EN[('Archives', 'fa-archive')] = [
+    ('By Publish', 'archives order by publish time', '/en/archives.html', 'fa-calendar'),
+    ('By Update', 'archives order by modify time', '/en/archives_updatedate.html', 'fa-pencil'),
+]
+
+I18N_SUBSITES = {
+    "en": {
+        "SITENAME": "Tech Notes of Yan Huang",
+        "HOMENAME": "Home",
+        "NIUX2_CATEGORY_MAP": {
+            'algorithm': ('Algorithm', 'fa-cogs'),
+            'research': ('Research', 'fa-flask'),
+            'note': ('Notes', 'fa-book'),
+            'life': ('Life', 'fa-coffee'),
+            'thinking': ('Thinking', 'fa-leaf'),
+            'collection': ('Collection', 'fa-briefcase'),
+        },
+        "NIUX2_HEADER_SECTIONS": [
+            ('Tags', 'Tags', '/en/tag/', 'fa-tags'),
+            ('About Me', 'About Me', '/en/AboutMe_en.html', 'fa-anchor'),
+            ('Site Home', 'Site HomePage', 'http://finaltheory.me/', 'fa-sitemap'),
+        ],
+        "NIUX2_HEADER_DROPDOWN_SECTIONS": NIUX2_HEADER_DROPDOWN_SECTIONS_EN,
+        "NIUX2_CATEGORY_TRANSL": 'Category'
+    }
+}
+
+I18N_UNTRANSLATED_ARTICLES = "hide"
+I18N_UNTRANSLATED_PAGES = "hide"
+
 
 # 下面这个dict定义了一组编译后不变的静态地址链接
 # 注意这里路径的写法，如果是在Linux下，要换成对应的风格！
@@ -116,7 +148,13 @@ READERS = {
 ###################################################################################################
 # 插件高级设置
 
-JINJA_EXTENSIONS = ['jinja2.ext.ExprStmtExtension',]
+JINJA_ENVIRONMENT = {
+    "extensions": [
+        "jinja2.ext.i18n",
+        "jinja2.ext.do",
+        "jinja2.ext.ExprStmtExtension"
+    ],
+}
 
 # sitemap plugin config
 SITEMAP = {
@@ -133,66 +171,50 @@ SITEMAP = {
     }
 }
 
-
-# extrac_headings plugin config
-def my_slugify(value, sep):
-    if type(value) == unicode:
-        # Fix for encoding errors
-        if 'nt' in os.name.lower():
-            value = value.encode('gb18030')
+def my_slugify(value, sep='-'):
+    if isinstance(value, str):
+        # Match old Windows vs Unix behavior
+        if os.name.lower().startswith('nt'):
+            value = value.encode('gb18030', errors='ignore')
         else:
-            value = value.encode('utf-8')
-    m = md5.new()
-    m.update(value)
-    return m.digest().encode('hex')[:5]
+            value = value.encode('utf-8', errors='ignore')
+    elif not isinstance(value, (bytes, bytearray)):
+        value = str(value).encode('utf-8', errors='ignore')
+
+    h = hashlib.md5()
+    h.update(value)
+    return h.hexdigest()[:8]
+
 MY_SLUGIFY_FUNC = my_slugify
 MY_HEADING_LIST_STYLE = 'ol'
 
-
-# niux2_lazyload_helper plugin config
-def my_img_url_2_path(url):
-    header = 'lazy:'
-    if not url.startswith(header):
-        print("ignore " + url)
-        return '', url
-    return os.path.abspath(os.path.join('content', url[1 + url.index('/', len(header)):])), url[len(header):]
-MY_IMG_URL2PATH_FUNC = my_img_url_2_path
-
-
-MD_EXTENSIONS = ([
-    'nl2br',
-    'extra',
-    'footnotes',
-    'tables',
-    codehilite.CodeHiliteExtension(configs=[('linenums', False), ('guess_lang', False)]),
-    headerid.HeaderIdExtension(configs=[('slugify', my_slugify)]),
-])
+MARKDOWN = {
+    "extensions": [
+        "markdown.extensions.meta",
+        "markdown.extensions.nl2br",
+        "markdown.extensions.extra",
+        "markdown.extensions.footnotes",
+        "markdown.extensions.tables",
+        "markdown.extensions.codehilite",
+    ],
+    "extension_configs": {
+        "markdown.extensions.codehilite": {
+            "linenums": False,
+            "guess_lang": False,
+        },
+    },
+    "output_format": "html5",
+}
 
 
 ###################################################################################################
-# 设置主题中的中文翻译
-NIUX2_AUTHOR_TRANSL = '作者'
-NIUX2_404_TITLE_TRANSL = '404错误'
-NIUX2_404_INFO_TRANSL = '请求的页面未找到！'
-NIUX2_TAG_TRANSL = '标签'
-NIUX2_ARCHIVE_TRANSL = '归档'
-NIUX2_ARCHIVE_UPDATEDATE_TRANSL = '归档（按修改时间）'
 NIUX2_CATEGORY_TRANSL = '分类'
-NIUX2_TAG_CLEAR_TRANSL = '清空'
-NIUX2_TAG_FILTER_TRANSL = '过滤标签'
-NIUX2_HEADER_TOC_TRANSL = '目录'
-NIUX2_SEARCH_TRANSL = '搜索'
-NIUX2_SEARCH_PLACEHOLDER_TRANSL = 'Google需要翻墙哦亲~'
-NIUX2_COMMENTS_TRANSL = '评论'
-NIUX2_PUBLISHED_TRANSL = '发布时间'
-NIUX2_LASTMOD_TRANSL = '最后修改'
-NIUX2_PAGE_TITLE_TRANSL = '页面'
-NIUX2_RECENT_UPDATE_TRANSL = '最近修改'
-NIUX2_HIDE_SIDEBAR_TRANSL = '隐藏侧边栏'
-NIUX2_SHOW_SIDEBAR_TRANSL = '显示侧边栏'
-NIUX2_REVISION_HISTORY_TRANSL = '修订历史'
-NIUX2_VIEW_SOURCE_TRANSL = '查看源文件'
-NIUX2_LAZY_LOAD_TEXT = '囧rz~努力加载中！'
+NIUX2_404_TITLE_TRANSL = '404 ERROR'
+NIUX2_404_INFO_TRANSL = 'Requested Page Not Found!'
+NIUX2_ARCHIVE_TRANSL = 'Archives'
+NIUX2_ARCHIVE_UPDATEDATE_TRANSL = 'Archives (By Update Time)'
+NIUX2_SEARCH_PLACEHOLDER_TRANSL = 'You probably need VPN to use Google!'
+NIUX2_LAZY_LOAD_TEXT = '囧rz~Loading!'
 
 # 其他与主题相关的设置
 
@@ -201,7 +223,7 @@ NIUX2_PYGMENTS_THEME = 'github'
 NIUX2_PAGINATOR_LENGTH = 11
 NIUX2_RECENT_UPDATE_NUM = 10
 NIUX2_FAVICON_URL = '/favicon.ico'
-NIUX2_GOOGLE_CSE_ID = '017413529027733437042:wvv9ghano_a'
+NIUX2_GOOGLE_CSE_ID = '8094c665b86c645ea'
 NIUX2_DISPLAY_TITLE = True
 NIUX2_LAZY_LOAD = True
 NIUX2_TOOLBAR = True
@@ -219,15 +241,15 @@ NIUX2_CATEGORY_MAP = {
 
 NIUX2_HEADER_SECTIONS = [
     ('标签', 'Tags', '/tag/', 'fa-tags'),
-    ('项目', 'My Projects', '/MyProjects.html', 'fa-rocket'),
+#    ('项目', 'My Projects', '/MyProjects.html', 'fa-rocket'),
     ('关于我', 'About Me', '/AboutMe.html', 'fa-anchor'),
     ('网站首页', 'Site HomePage', 'http://finaltheory.me/', 'fa-sitemap'),
 ]
 
 NIUX2_HEADER_DROPDOWN_SECTIONS = OrderedDict()
 NIUX2_HEADER_DROPDOWN_SECTIONS[('归档', 'fa-archive')] = [
-    ('归档 (按发布时间)', 'archives order by publish time', '/archives.html', 'fa-calendar'),
-    ('归档 (按修改时间)', 'archives order by modify time', '/archives_updatedate.html', 'fa-pencil'),
+    ('按发布时间', 'archives order by publish time', '/archives.html', 'fa-calendar'),
+    ('按更新时间', 'archives order by modify time', '/archives_updatedate.html', 'fa-pencil'),
 ]
 
 NIUX2_FOOTER_LINKS = [
@@ -235,11 +257,11 @@ NIUX2_FOOTER_LINKS = [
 ]
 
 NIUX2_FOOTER_ICONS = [
-    ('fa-wordpress', 'My WordPress Blog', 'http://wp.finaltheory.me'),
+    ('fa-linkedin', 'My Linkedin', 'https://www.linkedin.com/in/yan-huang-4061769b/'),
     ('fa-github', 'My Github Page', 'https://github.com/FinalTheory'),
-    ('fa-cloud', 'My CodeForces Page', 'http://codeforces.com/profile/FinalTheory'),
-    ('fa-renren', 'My RenRen Page', 'http://renren.com/FinalTheory'),
+    ('fa-instagram', 'My Instagram', 'https://www.instagram.com/yanhuang9421/'),
     ('fa-facebook-square', 'My Facebook Page', 'https://www.facebook.com/ForFinalTheory'),
+    ('fa-book', 'My RED Page', 'https://www.xiaohongshu.com/user/profile/5839b2e3a9b2ed428801e6c6'),
     ('fa-envelope-o', 'Send E-mail to Me', 'mailto: FinalTheory@hotmail.com'),
     ('fa-rss', 'Subscribe My Blog', '/feed.xml'),
 ]
--- publishconf.py
+++ publishconf.py
@@ -17,7 +17,7 @@ RELATIVE_URLS = False
 
 FEED_ALL_RSS = 'feed.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
